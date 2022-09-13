701a2f28721dee4ad3d2b36bacca8387d87c232c
Matheus <kroskamatheus@gmail.com>
2022-09-12 23:09:26 -0300

    ci(package.json/vite.config.ts): changing url name

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mhkroska-dev/',
+  base: '/portfolio/',
   test: {
     globals: true,
     environment: 'jsdom',
