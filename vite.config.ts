3f221c04878c6527d5cc6b518b9f5cdc67a314f2
Matheus <kroskamatheus@gmail.com>
2022-09-12 23:03:38 -0300

    ci(vite.config.ts): fix base url for deploy
    n

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/mhkroska-dev/',
   test: {
     globals: true,
     environment: 'jsdom',
